7fa19b5531c6e659fcaac9adfd6e98f49c252dfa
Arash <53490252+ThePythonist@users.noreply.github.com>
2021-11-08 15:26:58 +0330

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+name = "Sorena"
+age = 17
+code = "003245"
+print("Name : "+name)
+print("Age :",age)
+print("Student Code :",code)
---
+++ 4.py
@@ -0,0 +1,4 @@
+# age = input("Enter your age : ")
+# age = int(age)
+age = int(input("Enter your age : "))
+print(age/2)
