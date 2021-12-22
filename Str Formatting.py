181c8d14af24c3a2c03ed395ce504ff84f20ae23
Arash <53490252+ThePythonist@users.noreply.github.com>
2021-12-22 20:10:14 +0330

    Add files via upload

---
+++ Str Formatting.py
@@ -0,0 +1,17 @@
+name = "Kamal"
+age = 40
+birth = 1355
+
+# output = "Hello my name is {} and i am {} years old".format(name,age)
+# print(output)
+
+# output = "Hello my name is {n} and i am {a} years old".format(a=age,n=name,)
+# print(output)
+
+# output = f"Hello my name is {name} and i am {age} years old"
+# print(output)
+
+
+a,b = 10,20
+output = f"Ten plus twenty is equal to {a+b} and not {a*b}"
+print(output)
